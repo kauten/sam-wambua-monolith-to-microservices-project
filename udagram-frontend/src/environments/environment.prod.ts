bc33efd77406bba7cd40c48593885a2a2bab35b3
kauten <wambua.sam@gmail.com>
2022-07-25 00:31:02 +0300

    changed frontend to v4

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a2692540961bb40df81f6f69e6305b56-1465933646.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
