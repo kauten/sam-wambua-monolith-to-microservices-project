8760d1e891136dee680450512f755f4579c2632f
kauten <wambua.sam@gmail.com>
2022-07-27 00:28:22 +0300

    v10

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a79d2410a883443f29151f33ef029707-849436266.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
